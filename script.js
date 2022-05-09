2da1585886e3fc01744a76ffaa6007ef29225737
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2022-05-09 14:11:15 +0100

    info console logs with vue

--- script.js
+++ script.js
@@ -3,25 +3,24 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
+      riot_key: "RGAPI-bdacdafb-a31a-466d-bf68-116534bbfeac",
       title: "Summoner Account",
       summonerID: " ",
+      server: " ",
     };
   },
+  methods: {
+    searchForPlayer: function () {
+      const APICallString = `https://${this.server}.api.riotgames.com/lol/summoner/v4/summoners/by-name/${this.summonerID}?api_key=${this.riot_key}`;
+
+      fetch(APICallString)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data.name);
+          console.log(data.summonerLevel);
+          console.log(data);
+        })
+        .catch((error) => console.log(error));
+    },
+  },
 }).mount("#app");
-
-const riot_key = "RGAPI-bdacdafb-a31a-466d-bf68-116534bbfeac";
-
-function searchForPlayer(summonerName) {
-  const APICallString = `https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${riot_key}`;
-
-  fetch(APICallString)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data.name);
-      console.log(data.summonerLevel);
-      console.log(data);
-    })
-    .catch((error) => console.log(error));
-}
-
-searchForPlayer("JacketsAreWarm");
